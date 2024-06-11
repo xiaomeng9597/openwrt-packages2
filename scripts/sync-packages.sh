842d3774253bfe0d679d473a39eb607227592df6
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-06-11 17:11:49 +0800

    Add files via upload

--- scripts/sync-packages.sh
+++ scripts/sync-packages.sh
@@ -5,11 +5,11 @@ syncpath="./workdir"
 
 # 拉取luci-app-usb-printer软件包
 mkdir -p $syncpath/luci-app-usb-printer
-# git clone https://github.com/kenzok8/jell.git
+git clone https://github.com/kenzok8/jell.git
 # ls jell/luci-app-usb-printer
-# cp -a jell/luci-app-usb-printer/* $syncpath/luci-app-usb-printer/
+cp -a jell/luci-app-usb-printer/* $syncpath/luci-app-usb-printer/
 
-git clone https://github.com/immortalwrt/luci.git
+# git clone https://github.com/immortalwrt/luci.git
 # ls luci/applications/luci-app-usb-printer
-cp -a luci/applications/luci-app-usb-printer/* $syncpath/luci-app-usb-printer/
+# cp -a luci/applications/luci-app-usb-printer/* $syncpath/luci-app-usb-printer/
 # ls $syncpath/luci-app-usb-printer
