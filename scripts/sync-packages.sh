3d02576e37682e0a8180806ddd6a563ab05f585a
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-07-10 08:01:22 +0800

    Add files via upload

--- scripts/sync-packages.sh
+++ scripts/sync-packages.sh
@@ -2,7 +2,7 @@
 
 # 工作目录
 sudo mkdir -p /mnt/xiaomeng9597
-sudo chown $USER:$GROUPS /mnt/xiaomeng9597
+sudo chown "$USER:$GROUPS" /mnt/xiaomeng9597
 cd /mnt/xiaomeng9597
 syncpath="/mnt/xiaomeng9597/workdir"
 
